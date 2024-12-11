6daa3c91db2037c6ca04df74864734f26c0d1fc8
Luciano-Covella <luciano.covella@student.unisg.ch>
2024-12-11 15:41:06 +0100

    c

--- settings_page.py
+++ settings_page.py
@@ -1,42 +1,42 @@
 import streamlit as st
 
 # Initialization of session state variables
-if "flate_name" not in st.session_state:
-    st.session_state["flate_name"] = ""
-if "roommates" not in st.session_state:
+if "flate_name" not in st.session_state: #initlialize flat name if not set
+    st.session_state["flate_name"] = ""  
+if "roommates" not in st.session_state:  #initlialize roommate if not set
     st.session_state["roommates"] = []
 if "setup_finished" not in st.session_state: # use to get to the initial setup
     st.session_state["setup_finished"] = False
 
 # Function for app setup: Flat name
 def setup_flat_name():
-    st.title("🏠 Wasteless App - Setup")
-    flate_name = st.text_input("Please enter your flat name")
-    if st.button("Confirm flat name"): 
-        if flate_name:
-            st.session_state["flate_name"] = flate_name
-            st.success(f"You successfully saved your flate name: {flate_name}")
+    st.title("🏠 Wasteless App - Setup") #display the app title
+    flate_name = st.text_input("Please enter your flat name") #input field for flat name
+    if st.button("Confirm flat name"): #button to confirm flat name
+        if flate_name: #check if flat name was entered
+            st.session_state["flate_name"] = flate_name #save name to session state
+            st.success(f"You successfully saved your flate name: {flate_name}") #return message if successful
         else:
-            st.warning("Please enter a flat name")
+            st.warning("Please enter a flat name") #message if no flat name was entered
 
 # Function for app setup: rooomates
 def setup_roommates():
-    st.title(f"Welcome to {st.session_state['flate_name']}!")
-    room_mate = st.text_input("Please enter the name of a roommate", key="room_mate_input")
-    if st.button("Add a new roommate"):
-        add_roommate(room_mate)
-    display_roommates()
-    if st.button("Finish"):
-        st.success("Congratulations, your settings are done.")
-        st.session_state["setup_finished"] = True
+    st.title(f"Welcome to {st.session_state['flate_name']}!") #show flat name in title which was previsously set up
+    room_mate = st.text_input("Please enter the name of a roommate", key="room_mate_input")#input for name of roommates
+    if st.button("Add a new roommate"): #button to add roommate
+        add_roommate(room_mate) #call funktion to add roomate
+    display_roommates()#show list of current roommates
+    if st.button("Finish"): #button to finish setup
+        st.success("Congratulations, your settings are done.") #show a message
+        st.session_state["setup_finished"] = True #mark setup as finished
 
 # Function for adding a roommate
 def add_roommate(room_mate):
     if room_mate and room_mate not in st.session_state["roommates"]: # Checks if room_mate is not empty and not already in the list
-        st.session_state["roommates"].append(room_mate)
-        st.success(f"Roommate {room_mate} has been added!")
-    elif room_mate in st.session_state["roommates"]:
-        st.warning(f"Roommate {room_mate} is already in the list!")
+        st.session_state["roommates"].append(room_mate) #add roommate to list
+        st.success(f"Roommate {room_mate} has been added!") #return message that roommate has been succesfully added
+    elif room_mate in st.session_state["roommates"]: # if roomate already exists
+        st.warning(f"Roommate {room_mate} is already in the list!") #return warning message
 
 # Function to display the roommates
 def display_roommates():
